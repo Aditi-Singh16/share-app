ff34ba9a0aaf0e01ccedd930a6e6f99fa2f353e2
Aditi Singh <aditiansingh@gmail.com>
2021-07-17 14:04:22 +0530

    Firebase setup

---
+++ lib/screens/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:share_app/screens/receiver.dart';
+import 'package:share_app/screens/sender.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ShareApp'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            child: Text(
+                  'sender',
+                  style: TextStyle(color: Colors.white, fontSize: 40),
+                ),
+            onPressed: (){
+              Navigator.push(context,MaterialPageRoute(
+                builder: (BuildContext context) => SenderScreen('sender'),
+              ));
+
+              },
+            ),
+          ElevatedButton(
+            child: Text(
+                'receiver',
+                style: TextStyle(color: Colors.white, fontSize: 40),
+            ),
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(
+                builder: (BuildContext context) => ReceiverScreen('receiver'),
+              )
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
